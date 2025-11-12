ae5d7e027940ea7b3c2127c262dbc0780579edcb
Johannes Wirth <johannes.wirth.2@hof-university.de>
2025-11-12 15:06:38 +0100

    Added script for kaldi dataset handling.

---
+++ read_datasets.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+dataset_name = "/training-1/asr_dataset_files/asr_auf_zwei_planeten"
+
+split = "train_nodev" # train_dev test
+
+id_wav, id_text = {}, {}
+
+with open(f"{dataset_name}/wav.scp", encoding="UTF-8") as rf:
+    for line in rf:
+        id, wav_path = line.strip().split("\t")
+        id_wav[id] = wav_path
+
+with open(f"{dataset_name}/text", encoding="UTF-8") as rf:
+    for line in rf:
+        id, text = line.strip().split("\t")
+        id_text[id] = text
+
+for id in id_wav:
+    wav_path = id_wav[wav_path]
+    text = id_text
